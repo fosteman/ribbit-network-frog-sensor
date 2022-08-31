ec302c8b47e1da140f91fb6bd56d056650393701
fosteman <timfosteman@gmail.com>
2022-08-31 16:12:18 -0400

    modified install.sh

--- software/installation/install.sh
+++ software/installation/install.sh
@@ -2,6 +2,11 @@ sudo apt update
 sudo apt upgrade -y
 sudo apt install nginx hostapd dnsmasq net-tools python3-acme python3-certbot python3-mock python3-openssl python3-pkg-resources python3-pyparsing python3-zope.interface python3-certbot-nginx git -y
 
+# need latest node >v14
+sudo apt-get install npm
+sudo npm install -g n
+sudo n stable
+
 echo "Prepping dnsmasq.conf"
 cp ./dnsmasq.conf /etc/dnsmasq.conf
 
