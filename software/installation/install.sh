8d6f805f4cc9ce2b976dc02569c2e63bcdb5c231
fosteman <timfosteman@gmail.com>
2022-08-31 23:40:56 -0400

    another build

--- software/installation/install.sh
+++ software/installation/install.sh
@@ -1,22 +1,22 @@
 sudo apt update 
 sudo apt upgrade -y
-sudo apt install nginx hostapd dnsmasq net-tools python3-acme python3-certbot python3-mock python3-openssl python3-pkg-resources python3-pyparsing python3-zope.interface python3-certbot-nginx git -y
+sudo apt install nginx hostapd dnsmasq net-tools git -y
 
 # need latest node >v14
-sudo apt-get install npm
+sudo apt-get install npm -y
 sudo npm install -g n
 sudo n stable
 
 echo "Prepping dnsmasq.conf"
-cp ./dnsmasq.conf /etc/dnsmasq.conf
+sudo cp ./dnsmasq.conf /etc/dnsmasq.conf
 
-echo "Prepping nginx config"
-cp ./nginx.conf /etc/nginx/sites-enabled/default
+#echo "Prepping nginx config"
+#cp ./nginx.conf /etc/nginx/sites-enabled/default
 
 echo "Prepping hostapd"
 git clone https://github.com/oblique/create_ap
 cd create_ap
 sudo make install
 
-echo "Preparing frontend"
-cp -R ../server/dashboard/build/* /var/www/html/
+#echo "Preparing frontend"
+#cp -R ../server/dashboard/build/* /var/www/html/
--- software/installation/run_ap.sh
+++ software/installation/run_ap.sh
@@ -3,9 +3,11 @@ killall create_ap
 sudo service dnsmasq restart
 #sudo service nginx restart
 
-sudo service systemd-resolved stop
-sudo ifconfig wlan0 down
+# sudo service systemd-resolved stop
+# sudo ifconfig wlan0 down
 
 sudo create_ap -n wlan0 FROG --no-virt --no-dnsmasq --redirect-to-localhost --daemon
 
 sudo ifconfig wlan0 192.168.12.1
+
+sudo node ../server/build/index.js
