982d3e8721aa11ce5eb64fc913e7d71af761273d
fosteman <timfosteman@gmail.com>
2022-08-31 22:08:24 -0400

    update

--- software/installation/run_ap.sh
+++ software/installation/run_ap.sh
@@ -3,6 +3,8 @@ killall create_ap
 sudo service dnsmasq restart
 sudo service nginx restart
 
+sudo ifconfig wlan0 down
+
 sudo create_ap -n wlan0 FROG --no-virt --no-dnsmasq --redirect-to-localhost --daemon
 
 sudo ifconfig wlan0 192.168.12.1
